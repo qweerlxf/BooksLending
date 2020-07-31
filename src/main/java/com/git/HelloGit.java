c4e02652e53963d3022c153cb690a37615d86b1b
qweerlxf <50584583+qweerlxf@users.noreply.github.com>
2020-07-31 21:43:55 +0800

    在线更新

--- src/main/java/com/git/HelloGit.java
+++ src/main/java/com/git/HelloGit.java
@@ -4,10 +4,11 @@ public class HelloGit {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
-		System.out.println("更新1");
-		System.out.println("创建分支");
-		System.out.println("主干添加");
-		System.out.println("分支添加");
+		System.out.println("鏇存柊1");
+		System.out.println("鍒涘缓鍒嗘敮");
+		System.out.println("涓诲共娣诲姞");
+		System.out.println("鍒嗘敮娣诲姞");
+		System.out.println("鍦ㄧ嚎娣诲姞");
 	}
 	
 	
